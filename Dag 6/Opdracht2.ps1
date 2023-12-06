f68f439f4e566aec7ff43a319df707b92edee5a3
Gertjan Banga <gertjanb@delta-n.nl>
2023-12-06 15:48:32 +0100

    Update opdracht 2 dag 6

---
+++ Dag 6/Opdracht2.ps1
@@ -0,0 +1,14 @@
+$time=41667266
+$distancesRecord=244104712281040
+$press=6000000
+$distanceNewRecord=0
+
+while ($press -lt $time) {
+        $distance=($time-$press)*$press
+        If ($distance -gt $distancesRecord) {
+            $distanceNewRecord+=1
+            Write-Host $distanceNewRecord
+        }
+        $press+=1
+    }
+Write-Host "Antwoord:" $distanceNewRecord
